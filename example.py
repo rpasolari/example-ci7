104314d55cfdf9121f3ee70c3544301046f10c81
rpasolari <r.pasolari@gmail.com>
2022-03-31 10:25:09 +0200

    add subtraction test

--- example.py
+++ example.py
@@ -7,3 +7,11 @@ def test_add():
 	assert add(2, 3) == 5, "The addition of numbers is not correct"
 def test_add_strings():
 	assert add('space', 'ship') == "spaceship", "The addition of strings is not correct"
+
+def subtract(a, b):
+    return a + b  # <--- fix this in step 7
+
+
+# uncomment the following test in step 5
+def test_subtract():
+    assert subtract(2, 3) == -1
